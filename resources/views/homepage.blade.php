9db343753b0225e1b03f257cdd819e5f282a5fbb
Azuki-Azusa <cai980129@vip.qq.com>
2020-08-18 02:20:25 +0800

    GGBD

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="baidu-site-verification" content="g1pCqa4fFt"/>
     <meta name="google-site-verification" content="E4txdfp0AM3d3k9DSwH0J1yVynP2XJnyGfr0anDdWxI"/>
     <meta name="keywords" content="Azu機, BLOG, 博客, IT, 计算机, 编程, 假名, 歌词, 日文, 日文歌, 工具, 游戏, 手游">
     <meta name="Description" Content="Azu機个人网站，包括个人博客、日文歌词假名标注、游戏工具集、图床">
